9c73c8c567db03780d2c4faf40030393a74d5d00
Chris Cochrun <chris@tfcconnection.org>
2022-02-04 06:47:15 -0600

    first commit

---
+++ build/src/CMakeFiles/helloworld.dir/helloworld_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/helloworld.dir/helloworld_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
+ /home/chris/dev/newapp/build/src/helloworld_autogen/EWIEGA46WW/qrc_resources.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/src/CMakeFiles/helloworld.dir/helloworld_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/helloworld.dir/helloworld_autogen/mocs_compilation.cpp.o: \
+ /home/chris/dev/newapp/build/src/helloworld_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
