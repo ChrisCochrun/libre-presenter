e0f5310e379b4b5e5e4847bf9f02343fb9b3b21b
Chris Cochrun <chris@tfcconnection.org>
2022-02-22 09:26:37 -0600

    fixing build in git

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/dev/church-presenter/src
-# Build directory: /home/chris/dev/church-presenter/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
