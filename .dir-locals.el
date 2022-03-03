9993542d573357d2bbedcd133f5c618a333aa80b
Chris Cochrun <chris@tfcconnection.org>
2022-03-03 13:32:10 -0600

    removing projectile locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,2 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((projectile-project-run-cmd . "./build/bin/presenter")
-         (compilation-read-command . (nil))
-         (projectile-project-compilation-cmd . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build/ . && make --dir build/"))))
