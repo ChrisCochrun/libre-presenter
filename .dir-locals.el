e562a714e9ef9a926c508e78202b7f7626bec407
Chris Cochrun <chris@cochrun.xyz>
2022-10-06 03:34:58 -0500

    fix some dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((projectile-project-compilation-cmd . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build/ . && make -j8 --dir build/ && rm -rf ~/.cache/librepresenter/Libre Presenter/qmlcache/")
+((nil . ((projectile-project-compilation-cmd . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build/ . && make -j8 --dir build/ && rm -rf ~/.cache/librepresenter/Libre\ Presenter/qmlcache/")
          (projectile-project-run-cmd . "./build/bin/presenter")))
  (c++-mode . ((aggressive-indent-mode . nil))))
