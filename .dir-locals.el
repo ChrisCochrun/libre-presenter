7a1f98aeb42086be79f28f3eef19e7783906a95b
Chris Cochrun <chris@cochrun.xyz>
2022-12-08 15:22:07 -0600

    fixing .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,18 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-<<<<<<< HEAD
 ((nil . ((projectile-project-compilation-cmd . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -B bld/ . && make -j8 --dir bld/ && rm -rf ~/.cache/librepresenter/Libre\ Presenter/qmlcache/")
          (compile-command . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -B bld/ . && make -j8 --dir bld/ && rm -rf ~/.cache/librepresenter/Libre\ Presenter/qmlcache/")
-=======
-<<<<<<< HEAD
-((nil . ((projectile-project-compilation-cmd . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build/ . && make -j8 --dir bld/ && rm -rf ~/.cache/librepresenter/Libre\ Presenter/qmlcache/")
-         (compile-command . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build/ . && make -j8 --dir bld/ && rm -rf ~/.cache/librepresenter/Libre\ Presenter/qmlcache/")
->>>>>>> c978cf0 (updating)
-         (projectile-project-run-cmd . "./bld/bin/presenter")))
-=======
-((nil . ((projectile-project-compilation-cmd . "cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build/ . && make -j8 --dir build/ && rm -rf ~/.cache/librepresenter/Libre\ Presenter/qmlcache/")
-         (compile-command . "cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build/ . && make -j8 --dir build/ && rm -rf ~/.cache/librepresenter/Libre\ Presenter/qmlcache/")
-         (projectile-project-run-cmd . "./build/bin/presenter")))
->>>>>>> fb671cc (updating)
  (c++-mode . ((aggressive-indent-mode . nil))))
