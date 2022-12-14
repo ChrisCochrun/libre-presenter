a30993c0bef6a12dc52e43b0e1cbc26b563fb677
Chris Cochrun <chris@cochrun.xyz>
2022-12-14 14:19:06 -0600

    removing empty line

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use cxx_qt_build::CxxQtBuilder;
 
 fn main() {
-    // CxxQtBuilder::new().file("src/rust/my_object.rs").build();
     CxxQtBuilder::new()
         .file("src/rust/service_thing.rs")
         .file("src/rust/file_helper.rs")
