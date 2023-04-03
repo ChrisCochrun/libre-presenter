6f545c3bf8839824878faf2589894863284a7155
Chris Cochrun <chris@cochrun.xyz>
2023-04-03 14:56:44 -0500

    removing unused tests and unused video_thumbnail mod

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,17 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
 
 // mod my_object;
 mod file_helper;
@@ -19,3 +19,4 @@ mod service_thing;
 mod settings;
 mod slide_obj;
 mod slide_model;
+// mod video_thumbnail;
