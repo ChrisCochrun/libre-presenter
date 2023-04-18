8c89c49f4a1cbfd67397e5f5390d106c34e08ae0
Chris Cochrun <chris@cochrun.xyz>
2023-04-18 11:15:48 -0500

    cleanup lib.rs

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,19 +1,3 @@
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
-
-// mod my_object;
 mod file_helper;
 pub mod image_model;
 pub mod models;
@@ -24,5 +8,4 @@ mod settings;
 mod slide_model;
 mod slide_obj;
 pub mod video_model;
-// mod entities;
 // mod video_thumbnail;
