3f00cf7abd5163aeb9c328e27284aa6c1beceae0
Chris Cochrun <chris@cochrun.xyz>
2023-07-08 21:45:01 -0500

    removing things and moving back to nix

--- build.rs
+++ build.rs
@@ -5,7 +5,6 @@ use std::path::{Path, PathBuf};
 fn main() {
     // let guix_profile_dir = env::var("GUIX_ENVIRONMENT").is_ok();
     // println!("{}", guix_profile_dir);
-    println!("cargo:CXXBRIDGE_DIR6=include/qt5");
     // let qt_include = PathBuf::from(guix_profile_dir).push("include").push("qt5");
     CxxQtBuilder::new()
         .file("src/rust/service_thing.rs")
@@ -19,12 +18,12 @@ fn main() {
         .file("src/rust/presentation_model.rs")
         .file("src/rust/song_model.rs")
         .file("src/rust/ytdl.rs")
-        .cc_builder(|cc| {
-            cc.include("/gnu/store/pkjvij1f6rvx42xv2kygicr7fsch41dl-profile/include/qt5/QtQml/QQmlApplicationEngine");
-            cc.include("/gnu/store/pkjvij1f6rvx42xv2kygicr7fsch41dl-profile/include/qt5/QtQml/qqmlapplicationengine.h");
-cc.include("/gnu/store/pkjvij1f6rvx42xv2kygicr7fsch41dl-profile/include/qt5/QtQml/qqmlengine.h");
-cc.include("/gnu/store/pkjvij1f6rvx42xv2kygicr7fsch41dl-profile/include/qt5/QtQml/QQmlEngine");
-cc.include("include/qt5");
-        })
+        //         .cc_builder(|cc| {
+        //             cc.include("/gnu/store/pkjvij1f6rvx42xv2kygicr7fsch41dl-profile/include/qt5/QtQml/QQmlApplicationEngine");
+        //             cc.include("/gnu/store/pkjvij1f6rvx42xv2kygicr7fsch41dl-profile/include/qt5/QtQml/qqmlapplicationengine.h");
+        // cc.include("/gnu/store/pkjvij1f6rvx42xv2kygicr7fsch41dl-profile/include/qt5/QtQml/qqmlengine.h");
+        // cc.include("/gnu/store/pkjvij1f6rvx42xv2kygicr7fsch41dl-profile/include/qt5/QtQml/QQmlEngine");
+        // cc.include("include/qt5");
+        //         })
         .build();
 }
